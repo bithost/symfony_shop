30e6c4544a83e54c554f30648cee9e78650d3247
Tadas <tadasa@gmail.com>
2018-02-18 12:46:03 +0200

    Configured to get working doctrine:migration.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             //PaginatorBundle
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
