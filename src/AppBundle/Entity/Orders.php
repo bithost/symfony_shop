70c324737ba7744ca36355e9784bd83354caf381
Tadas <tadasa@gmail.com>
2018-02-18 12:44:28 +0200

    Oders entity

---
+++ src/AppBundle/Entity/Orders.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Orders
+ *
+ * @ORM\Table(name="orders")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\OrdersRepository")
+ */
+class Orders
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="customer_name", type="string", length=255)
+     */
+    private $customerName;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="total_price", type="integer")
+     */
+    private $totalPrice;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="order_status", type="integer")
+     */
+    private $orderStatus;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set customerName
+     *
+     * @param string $customerName
+     *
+     * @return Orders
+     */
+    public function setCustomerName($customerName)
+    {
+        $this->customerName = $customerName;
+
+        return $this;
+    }
+
+    /**
+     * Get customerName
+     *
+     * @return string
+     */
+    public function getCustomerName()
+    {
+        return $this->customerName;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return Orders
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set totalPrice
+     *
+     * @param integer $totalPrice
+     *
+     * @return Orders
+     */
+    public function setTotalPrice($totalPrice)
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get totalPrice
+     *
+     * @return int
+     */
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    /**
+     * Set orderStatus
+     *
+     * @param integer $orderStatus
+     *
+     * @return Orders
+     */
+    public function setOrderStatus($orderStatus)
+    {
+        $this->orderStatus = $orderStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get orderStatus
+     *
+     * @return int
+     */
+    public function getOrderStatus()
+    {
+        return $this->orderStatus;
+    }
+}
+
